d05c0fa9245f1116319991995baacda62bb2b3cf
notrichardpeng <58278719+notrichardpeng@users.noreply.github.com>
2020-05-05 21:13:55 -0400

    Added selenium driver for clicking buttons

--- dictionary.py
+++ dictionary.py
@@ -1,7 +1,9 @@
 import requests
+import os
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
-def Definition(word):
+def Definition(word, maxTypeCount, maxDefCount):
     raw = requests.get('https://www.dictionary.com/browse/' + word).content
     soup = BeautifulSoup(raw, 'lxml')
     targetDiv = soup.find('div', {'class': 'css-1urpfgu e16867sm0'})
@@ -9,15 +11,28 @@ def Definition(word):
     allTypes = targetDiv.find_all('span', {'class': 'luna-pos'})
     allContents = targetDiv.find_all('div', {'class': 'css-1o58fj8 e1hk9ate4'})
 
-    for t in range(min(2, len(allTypes))):
+    for t in range(min(maxTypeCount, len(allTypes))):
         type = allTypes[t].text if allTypes[t].text[-1] != ',' else allTypes[t].text[:-1]
         content = allContents[t]
         defs = content.find_all('div', value=True)
 
         print(type)
-        for n in range(min(2, len(defs))):
+        for n in range(min(maxDefCount, len(defs))):
             print(str(n + 1) + '. ' + defs[n].find('span').text)
         print()
+
+
+
+def Synonym(word):
+    driverOpt = webdriver.ChromeOptions()
+    driverOpt.add_experimental_option('excludeSwitches', ['enable-logging'])
+    driverOpt.add_argument('--headless')
+    driver = webdriver.Chrome(os.path.dirname(os.path.realpath('dictionary.py')) + '\\chromedriver.exe', options=driverOpt)
+    driver.get('https://www.thesaurus.com/browse/' + word)
+
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    listOfVariations = soup.find('ul', {'class':'css-1wm7jhc e9i53te2'}).find_all('li')
+    print(listOfVariations)
 
 # main loop
 while True:
@@ -34,9 +49,8 @@ while True:
     word = parts[1]
 
     if command == 'def':
-        Definition(word)
+        Definition(word, 2, 2)
     elif command == 'syn':
-        raw = requests.get('https://www.thesaurus.com/browse/' + word).content
-        soup = BeautifulSoup(raw, 'lxml')
+        Synonym(word)
     else:
         print('invalid syntax')
