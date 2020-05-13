2f506866526410cd3a44bb12ec7600892a2c30a2
notrichardpeng <58278719+notrichardpeng@users.noreply.github.com>
2020-05-12 20:59:24 -0400

    Added optimizations to code grammar and selenium driver reusage. Readjusted variable names.

--- dictionary.py
+++ dictionary.py
@@ -1,83 +1,69 @@
 import requests
 import os
+import sys
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
-def Definition(word, maxTypeCount, maxDefCount):
-    raw = requests.get('https://www.dictionary.com/browse/' + word).content
+def definition(w, max_type_count, max_def_count):
+    raw = requests.get('https://www.dictionary.com/browse/' + w).content
     soup = BeautifulSoup(raw, 'lxml')
-    targetDiv = soup.find('div', {'class': 'css-1urpfgu e16867sm0'})
+    target_div = soup.find('div', {'class': 'css-1urpfgu e16867sm0'})
 
-    allTypes = targetDiv.find_all('span', {'class': 'luna-pos'})
-    allContents = targetDiv.find_all('div', {'class': 'css-1o58fj8 e1hk9ate4'})
+    all_types = target_div.find_all('span', {'class': 'luna-pos'})
+    all_contents = target_div.find_all('div', {'class': 'css-1o58fj8 e1hk9ate4'})
 
-    if not allTypes:
-        print('no results found for ' + word)
-        print('')
+    if not all_types:
+        print('no results found for ' + w)
         return
 
-    for t in range(min(maxTypeCount, len(allTypes))):
-        type = allTypes[t].text if allTypes[t].text[-1] != ',' else allTypes[t].text[:-1]
-        content = allContents[t]
+    n = min(max_type_count, len(all_types))
+    for t in range(n):
+        type = all_types[t].text if all_types[t].text[-1] != ',' else all_types[t].text[:-1]
+        content = all_contents[t]
         defs = content.find_all('div', value=True)
 
         print(type)
-        for n in range(min(maxDefCount, len(defs))):
-            print(str(n + 1) + '. ' + defs[n].find('span').text)
-        print()
+        for d in range(min(max_def_count, len(defs))):
+            print(str(d + 1) + '. ' + defs[d].find('span').text)
+
+        if t < n-1:
+            print('')
 
 
 
-def Synonym(word, maxSynCount):
-    driverOpt = webdriver.ChromeOptions()
-    driverOpt.add_experimental_option('excludeSwitches', ['enable-logging'])
-    driverOpt.add_argument('--headless')
-    driverOpt.add_argument("--proxy-server='direct://'")
-    driverOpt.add_argument("--proxy-bypass-list=*")
+def synonym(w, max_syn_count):
 
-    driverPath = 'C:\\Users\\notri\\Downloads\\chromedriver.exe'
-    try:
-        driver = webdriver.Chrome(os.path.dirname(__file__) + '\\chromedriver.exe', options=driverOpt)
-    except:
-        try:
-            driver = webdriver.Chrome(driverPath, options=driverOpt)
-        except:
-            print('Cannot locate chromdriver.exe')
-            return
-
-    driver.get('https://www.thesaurus.com/browse/' + word)
-    nextButton = None
+    driver.get('https://www.thesaurus.com/browse/' + w)
+    next_button = None
 
     try:
-        nextButton = driver.find_element_by_css_selector("button[href='#']")
+        next_button = driver.find_element_by_css_selector("button[href='#']")
     except:
         pass
 
-    if(nextButton):
+    if next_button:
         counter = 1
-        while(nextButton):
+        while next_button:
             soup = BeautifulSoup(driver.page_source, 'lxml')
 
-            currentActive = soup.find('li', {'class':'active-postab css-auzxq6-PosTab e9i53te0'})
-            print(str(counter) + '. ['+ currentActive.find('em').text + '] ' + currentActive.find('strong').text, end=': ')
-            listOfSyn = soup.find('ul', {'class':'css-17d6qyx-WordGridLayoutBox et6tpn80'}).find_all('li')
+            current_active = soup.find('li', {'class':'active-postab css-auzxq6-PosTab e9i53te0'})
+            print(str(counter) + '. ['+ current_active.find('em').text + '] ' + current_active.find('strong').text, end=': ')
+            list_of_syn = soup.find('ul', {'class':'css-17d6qyx-WordGridLayoutBox et6tpn80'}).find_all('li')
 
-            n = min(maxSynCount, len(listOfSyn))
+            n = min(max_syn_count, len(list_of_syn))
             for i in range(n):
-                if i < n-1: print(listOfSyn[i].find('a').text, end=" | ")
-                else: print(listOfSyn[i].find('a').text)
+                if i < n-1: print(list_of_syn[i].find('a').text, end=" | ")
+                else: print(list_of_syn[i].find('a').text)
 
             counter += 1
             try:
-                nextButton.click()
+                next_button.click()
             except:
                 break
-        print('')
     else:
-        variations = None
         try:
             variations = driver.find_element_by_css_selector("ul[class='css-z1dbbs-TabList e9i53te2']")
             variations = variations.find_elements_by_tag_name('li')
@@ -92,17 +78,28 @@ def Synonym(word, maxSynCount):
             meaning = v.find_element_by_tag_name('strong').text
             v.click()
 
-            listOfSyn = driver.find_element_by_css_selector("ul[class='css-17d6qyx-WordGridLayoutBox et6tpn80']")
-            listOfSyn = listOfSyn.find_elements_by_tag_name('li')
+            list_of_syn = driver.find_element_by_css_selector("ul[class='css-17d6qyx-WordGridLayoutBox et6tpn80']")
+            list_of_syn = list_of_syn.find_elements_by_tag_name('li')
 
             print(str(counter) + '. [' + reason + '] ' + meaning, end=': ')
-            n = min(maxSynCount, len(listOfSyn))
+            n = min(max_syn_count, len(list_of_syn))
             for i in range(n):
-                if i < n-1: print(listOfSyn[i].find_element_by_tag_name('a').text, end=' | ')
-                else: print(listOfSyn[i].find_element_by_tag_name('a').text)
+                if i < n-1: print(list_of_syn[i].find_element_by_tag_name('a').text, end=' | ')
+                else: print(list_of_syn[i].find_element_by_tag_name('a').text)
             counter += 1
 
-        print('')
+
+driver_opt = webdriver.ChromeOptions()
+driver_opt.add_experimental_option('excludeSwitches', ['enable-logging'])
+driver_opt.add_argument('--headless')
+driver_opt.add_argument("--proxy-server='direct://'")
+driver_opt.add_argument("--proxy-bypass-list=*")
+
+try:
+    driver = webdriver.Chrome(os.path.dirname(__file__) + '\\chromedriver.exe', options=driver_opt)
+except:
+    print('Cannot locate chromedriver.exe in the directory of this file')
+    sys.exit(0)
 
 
 # main loop
@@ -119,14 +116,15 @@ while True:
     command = parts[0]
     word = parts[1]
 
-    if(';' in word):
-        print('no results found for ' + word)
-        print('')
+    if ';' in word:
+        print('no results found for ' + word + '\n')
         continue
 
     if command == 'def':
-        Definition(word, 2, 2)
+        definition(word, 2, 2)
     elif command == 'syn':
-        Synonym(word, 2)
+        synonym(word, 2)
     else:
-        print('unknown command\n')
+        print('unknown command')
+
+    print('')
