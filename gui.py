2eb1cf23e994da30ea3fdf75ed500f757eb2471c
notrichardpeng <58278719+notrichardpeng@users.noreply.github.com>
2020-05-21 20:05:14 -0400

    Attempted to resize scrollbar to canvas size

--- gui.py
+++ gui.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+import dictionary
 
 root = Tk()
 root.geometry('600x600')
@@ -12,16 +12,18 @@ def search(input, output):
 def on_frame_configure(canvas):
     canvas.configure(scrollregion=canvas.bbox('all'))
 
+def configure_canvas_width(event):
+    output_canvas.itemconfig(canvas_window, width=event.width)
+
 output_canvas = Canvas(root, height=400, bd=1, relief=SUNKEN)
-output_frame = Frame(output_canvas)
-scroll = Scrollbar(root, command=output_canvas.yview)
+output_frame = Frame(output_canvas, bg='red')
+scroll = Scrollbar(output_frame, command=output_canvas.yview)
 output_canvas.configure(yscrollcommand=scroll.set)
 
 scroll.pack(side=RIGHT, fill=Y)
 output_canvas.pack(side=TOP, fill=X)
-output_canvas.create_window((4,4), window=output_frame, anchor="nw")
-
-
+canvas_window = output_canvas.create_window((0,0), window=output_frame, anchor="nw")
+output_canvas.itemconfig(canvas_window)
 
 input = Entry(root, width=35)
 input.pack(pady=5)
@@ -38,5 +40,6 @@ syn_button.pack(side=LEFT, padx=5)
 
 root.bind('<Return>', (lambda event: search(input, output_frame)))
 output_frame.bind("<Configure>", lambda event: on_frame_configure(output_canvas))
+output_canvas.bind("<Configure>", lambda event: configure_canvas_width(event))
 
 root.mainloop()
