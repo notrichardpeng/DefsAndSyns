d36b1ef068f7d2ba2f764b5b2dc20dd111fc229d
notrichardpeng <58278719+notrichardpeng@users.noreply.github.com>
2020-05-22 12:35:44 -0400

    Correctly adjusted scrollbar of output canvas

--- gui.py
+++ gui.py
@@ -12,18 +12,17 @@ def search(input, output):
 def on_frame_configure(canvas):
     canvas.configure(scrollregion=canvas.bbox('all'))
 
-def configure_canvas_width(event):
-    output_canvas.itemconfig(canvas_window, width=event.width)
+main_frame = Frame(root, height=400, bg='black')
+main_frame.pack(fill=X)
 
-output_canvas = Canvas(root, height=400, bd=1, relief=SUNKEN)
-output_frame = Frame(output_canvas, bg='red')
-scroll = Scrollbar(output_frame, command=output_canvas.yview)
-output_canvas.configure(yscrollcommand=scroll.set)
-
+output_canvas = Canvas(main_frame, height=400, bd=1, relief=SUNKEN)
+scroll = Scrollbar(main_frame, command=output_canvas.yview)
 scroll.pack(side=RIGHT, fill=Y)
 output_canvas.pack(side=TOP, fill=X)
-canvas_window = output_canvas.create_window((0,0), window=output_frame, anchor="nw")
-output_canvas.itemconfig(canvas_window)
+
+output_frame = Frame(output_canvas)
+output_canvas.configure(yscrollcommand=scroll.set)
+output_canvas.create_window((0,0), window=output_frame, anchor="nw")
 
 input = Entry(root, width=35)
 input.pack(pady=5)
@@ -40,6 +39,5 @@ syn_button.pack(side=LEFT, padx=5)
 
 root.bind('<Return>', (lambda event: search(input, output_frame)))
 output_frame.bind("<Configure>", lambda event: on_frame_configure(output_canvas))
-output_canvas.bind("<Configure>", lambda event: configure_canvas_width(event))
 
 root.mainloop()
